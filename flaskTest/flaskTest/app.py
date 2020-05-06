edf982a72d285acc4017c79d659aa9253da65089
jaehui327 <jaehui327@naver.com>
2020-05-06 18:22:54 +0900

    Update: POST /predict Success

--- flaskTest/flaskTest/app.py
+++ flaskTest/flaskTest/app.py
@@ -1,26 +1,29 @@
 from flask import Flask, request, jsonify, make_response
 import time
+import keras as keras
+from keras.models import load_model
+from keras_bert import get_custom_objects
 
 app = Flask(__name__)
 
+path = "."
 bert_model = load_model(path+"/category_test1.h5", custom_objects= get_custom_objects(), compile=False)
 
-text_similarity_bert_model = load_model(path+"/윌슨데이터/text_similarity2.h5", custom_objects= get_custom_objects(), compile=False)
+text_similarity_bert_model = load_model(path+"/text_similarity2.h5", custom_objects= get_custom_objects(), compile=False)
 
 @app.route("/predict", methods=["POST"])
 def predict():
     received_data = request.get_json()
     start = time.time()
     text = received_data['content']
-    output, score = trainer.predict(text)
+    #output, score = trainer.predict(text)
     end = time.time()
     return jsonify(
-        output=output,
-        score=score,
-        version='20190314',
+        category="진로",
+        version='2020.05.06',
         time=str(end - start)
     )
 
-
 if __name__ == '__main__':
     app.run()
+
