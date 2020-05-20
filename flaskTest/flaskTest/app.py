90084ed3a45c534e2f24a917718a6ff83ce3b399
jaehui327 <jaehui327@naver.com>
2020-05-21 05:29:31 +0900

    Update: 속도 개선 성공 ! - session

--- flaskTest/flaskTest/app.py
+++ flaskTest/flaskTest/app.py
@@ -9,6 +9,7 @@ import re
 import pickle
 
 import keras as keras
+from tensorflow.python.keras.backend import set_session
 from keras.models import load_model
 from keras import backend as K
 from keras import Input, Model
@@ -31,8 +32,6 @@ from dbconnect import connection
 
 import copy
 
-app = Flask(__name__)
-
 path = "."
 SEQ_LEN = 300
 
@@ -46,12 +45,6 @@ subcategories = pd.read_sql("SELECT * FROM willson_test.willsoner_subcategory", 
 willsoner = pd.read_sql("SELECT * FROM willson_test.willsoner", conn)
 willsoner = willsoner[['idx', 'experience']]
 
-'''
-love = data.loc[data['label'] == 0]
-course = data.loc[data['label'] == 1]
-self_esteem = data.loc[data['label'] == 2]
-relationship = data.loc[data['label'] == 4]
-'''
 love = willsoner.loc[subcategories['subcategory_idx'] < 5]
 love['category'] = '연애'
 love['label'] = 0
@@ -144,70 +137,93 @@ def predict_load_data1(requester, pandas_dataframe):
 
     return data_x
 
+app = Flask(__name__)
+# 카테고리 예측
+global bert_model
+bert_model = load_model(
+    path + "/category_test.h5",
+    custom_objects=get_custom_objects(),
+    compile=False)
+print('bert_model loaded')
+
+# 유사도 분석
+global text_similarity_bert_model
+text_similarity_bert_model = load_model(
+    path + "/text_similarity.h5",
+    custom_objects=get_custom_objects(),
+    compile=False)
+print('text_similarity_bert_model loaded')
+
+global session
+global graph
+session = keras.backend.get_session()
+init = tf.global_variables_initializer()
+session.run(init)
+graph = tf.get_default_graph()
+
+# bert_model = None
+# text_similarity_bert_model = None
+# graph = None
+
+# def load_model():
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
     received_data = request.get_json()
     start = time.time()
     text = received_data['content']
+    with session.as_default():
+        with graph.as_default():
+            set_session(session)
+            # 카테고리 예측
+            new_data = predict_load_data(text)
 
-    # 카테고리 예측
-    bert_model = load_model(
-        path + "/category_test.h5",
-        custom_objects=get_custom_objects(),
-        compile=False)
+            preds = bert_model.predict(new_data)
+            percent = np.max(preds) * 100
+            percent = round(percent, 2)
+            preds = np.argmax(preds, axis=1)  # 가장 높은 인덱스 추출
+            label = preds.tolist()  # numpy to list
 
-    new_data = predict_load_data(text)
+            category = ''
+            if label[0] == 0:
+                category = '연애'
+            elif label[0] == 1:
+                category = '진로'
+            elif label[0] == 2:
+                category = '자존감'
+            elif label[0] == 3:
+                category = '일상'
+            else:
+                category = '대인관계'
 
-    preds = bert_model.predict(new_data)
-    percent = np.max(preds) * 100
-    percent = round(percent, 2)
-    preds = np.argmax(preds, axis=1)  # 가장 높은 인덱스 추출
-    label = preds.tolist()  # numpy to list
+            # 유사도 분석
+            sorted_category = data_category[label[0]]
+            select_category = sorted_category.reset_index(drop=True)  # 인덱스 reset
 
-    category = ''
-    if label[0] == 0:
-        category = '연애'
-    elif label[0] == 1:
-        category = '진로'
-    elif label[0] == 2:
-        category = '자존감'
-    elif label[0] == 3:
-        category = '일상'
-    else:
-        category = '대인관계'
+            similarity_set = predict_load_data1(text, select_category)  # 문장 유사도를 위한 버트 input 데이터 생성
 
-    # 유사도 분석
-    text_similarity_bert_model = load_model(
-        path + "/text_similarity.h5",
-        custom_objects=get_custom_objects(),
-        compile=False)
+            text_similarity_preds = text_similarity_bert_model.predict(similarity_set)
 
-    sorted_category = data_category[label[0]]
-    select_category = sorted_category.reset_index(drop=True)  # 인덱스 reset
+            preds = copy.deepcopy(text_similarity_preds)
+            text_similarity_rank = []
+            result = []
+            result_dictionary = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
+            result_dictionary1 = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
+            result_dictionary2 = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
+            dic_list = [result_dictionary, result_dictionary1, result_dictionary2]
+            for i in range(3):
+                x = np.argmax(preds)
+                text_similarity_rank.append(x)
+                print(select_category[x:x + 1])
+                result.append(select_category[x:x + 1])
+                preds[text_similarity_rank[i]] = [0]
+                dic_list[i]['willsoner_idx'] = int(result[i].iloc[0, 0])
+                dic_list[i]['experience'] = result[i].iloc[0, 1]
+                dic_list[i]['category'] = result[i].iloc[0, 2]
+                dic_list[i]['label'] = int(result[i].iloc[0, 3])
 
-    similarity_set = predict_load_data1(text, select_category)  # 문장 유사도를 위한 버트 input 데이터 생성
-
-    text_similarity_preds = text_similarity_bert_model.predict(similarity_set)
-
-    preds = copy.deepcopy(text_similarity_preds)
-    text_similarity_rank = []
-    result = []
-    result_dictionary = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
-    result_dictionary1 = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
-    result_dictionary2 = {'willsoner_idx': -1, 'experience': ' ', 'category': ' ', 'label': -1}
-    dic_list = [result_dictionary, result_dictionary1, result_dictionary2]
-    for i in range(3):
-        x = np.argmax(preds)
-        text_similarity_rank.append(x)
-        print(select_category[x:x + 1])
-        result.append(select_category[x:x + 1])
-        preds[text_similarity_rank[i]] = [0]
-        dic_list[i]['willsoner_idx'] = int(result[i].iloc[0, 0])
-        dic_list[i]['experience'] = result[i].iloc[0, 1]
-        dic_list[i]['category'] = result[i].iloc[0, 2]
-        dic_list[i]['label'] = int(result[i].iloc[0, 3])
-
-    end = time.time()
+            end = time.time()
 
     # 예측 결과
     return jsonify({
@@ -226,45 +242,9 @@ def predict():
         }
         })
 
-@app.route("/test", methods=["GET", "POST"])
-def test():
-    try:
-        return jsonify({
-            'data' : data_category
-        })
-
-    except Exception as e:
-        return (str(e))
-
-'''willsoner
-    {
-  "data": [
-    [
-      1,
-      9,
-      "기기기",
-      "가가곡",
-      7,
-      4.57143,
-      1586327606,
-      1586327606,
-      1586327606,
-      3,
-      null
-      ], ...
-'''
-'''willsoner_subcategory
-    {
-  "data": [
-    [
-      1,
-      1586327606,
-      1586327606,
-      1,
-      6
-    ],
-'''
 if __name__ == '__main__':
+    # load_model()
+
     app.run(host='192.168.219.109', debug=True, port="50051")
     # application.debug = True
     # application.run()
